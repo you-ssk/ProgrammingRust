7bf35063fb2ffa43c51cd2b1dd09015a990d3169
Yo Sasaki <you_ssk@outlook.com>
2023-08-06 22:32:16 +0900

    add post_gcd

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,9 +1,17 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
 use serde::Deserialize;
 
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
 fn main() {
     let server = HttpServer::new(|| {
-        App::new().route("/", web::get().to(get_index))
+        App::new()
+        .route("/", web::get().to(get_index))
+        .route("/gcd", web::post().to(post_gcd))
     });
 
     println!("serving on http://localhost:3000");
@@ -16,11 +24,35 @@ fn get_index() -> HttpResponse {
     HttpResponse::Ok().content_type("text/html").body(
         r#"
         <title>GCD Calculator</title>
-        <form action"/gcd" method="post">
+        <form action="/gcd" method="post">
         <input type="text" name="n"/>
         <input type="text" name="m"/>
         <button type="submit">Compute GCD</button>
         </form>
         "#,
     )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest().content_type("text/html").body("Computing the GCD zero is boring");
+    }
+
+    let response = format!("The greatest common divisor of the numbers {} and {} is <b>{}</b>\n",
+    form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok().content_type("text/html").body(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
