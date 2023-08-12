ed5f325a63f9ea72c629d1412c060de650d5b1a4
Yo Sasaki <inukichi.i@gmail.com>
2023-08-12 18:26:39 +0900

    add parse complex

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -21,7 +21,7 @@ fn square_add_loop(c: f64) {
 fn complex_square_add_loop(c: Complex<f64>) {
     let mut z = Complex { re: 0.0, im: 0.0 };
     loop {
-        z = z * z  + c;
+        z = z * z + c;
     }
 }
 
@@ -40,11 +40,18 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
         None => None,
         Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index +1..])) {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
                 (Ok(l), Ok(r)) => Some((l, r)),
                 _ => None
             }
         }
+    }
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair::<f64>(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
     }
 }
 
@@ -53,8 +60,22 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("", ','), None);
     assert_eq!(parse_pair::<i32>("10,", ','), None);
     assert_eq!(parse_pair::<i32>(",10", ','), None);
-    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10,20)));
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
     assert_eq!(parse_pair::<i32>("0.5x", 'x'), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+    assert_eq!(parse_complex(",-0.0625"), None);
+    assert_eq!(parse_complex("1,-0.0625"), Some(Complex{re: 1f64, im:-0.0625}));
+    assert_eq!(parse_complex("1,-0.0625"), Some(Complex{re: 1., im:-0.0625}));
 }
