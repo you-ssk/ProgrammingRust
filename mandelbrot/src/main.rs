d2306149273d7f3e7e529cef6ea964c55314cbeb
Yo Sasaki <[inukichi.i@gmail.com]>
2023-08-12 14:09:25 +0900

    add Complex and loop(temporary)

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn square_loop(mut x: f64) {
+    loop {
+        x = x * x;
+    }
+}
+
+fn square_add_loop(c: f64) {
+    let mut x = 0.;
+    loop {
+        x = x * x + c;
+    }
+}
+
+use num::Complex;
+
+fn complex_square_add_loop(c: Complex<f64>) {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    loop {
+        z = z * z  + c;
+    }
+}
