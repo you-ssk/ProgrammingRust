4f59e08f3ed30f9725b113cd92eb5ede424f552c
Yo Sasaki <inukichi.i@gmail.com>
2023-08-13 16:02:06 +0900

    2.6.3, 2.6.4

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -39,12 +39,10 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
         None => None,
-        Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
-                (Ok(l), Ok(r)) => Some((l, r)),
-                _ => None
-            }
-        }
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
     }
 }
 
@@ -52,6 +50,22 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
     match parse_pair::<f64>(s, ',') {
         Some((re, im)) => Some(Complex { re, im }),
         None => None,
+    }
+}
+
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
     }
 }
 
@@ -76,6 +90,53 @@ fn test_parse_complex() {
         })
     );
     assert_eq!(parse_complex(",-0.0625"), None);
-    assert_eq!(parse_complex("1,-0.0625"), Some(Complex{re: 1f64, im:-0.0625}));
-    assert_eq!(parse_complex("1,-0.0625"), Some(Complex{re: 1., im:-0.0625}));
+    assert_eq!(
+        parse_complex("1,-0.0625"),
+        Some(Complex {
+            re: 1f64,
+            im: -0.0625
+        })
+    );
+    assert_eq!(
+        parse_complex("1,-0.0625"),
+        Some(Complex {
+            re: 1.,
+            im: -0.0625
+        })
+    );
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 200),
+            (25, 175),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
+        ),
+        Complex {
+            re: -0.5,
+            im: -0.75
+        }
+    );
+}
+
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            }
+        }
+    }
 }
