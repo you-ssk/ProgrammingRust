5de5e9fc5e0584a6d95438fe1984be6095b03435
Yo Sasaki <inukichi.i@gmail.com>
2023-08-14 11:35:45 +0900

    format

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -105,13 +105,15 @@ fn render_p(
                 let height = band.len() / bounds.0;
                 let band_bounds = (bounds.0, height);
                 let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
-                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
 
                 spawner.spawn(move |_| {
                     render(band, band_bounds, band_upper_left, band_lower_right);
                 });
             }
-        }).unwrap();
+        })
+        .unwrap();
     }
 }
 
