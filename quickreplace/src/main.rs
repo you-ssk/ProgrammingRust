726623b683e90539bf1fd408372a50ef2865c42a
Yo Sasaki <inukichi.i@gmail.com>
2023-08-14 15:44:58 +0900

    2.7.2

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,6 +1,33 @@
+use std::fs;
+
 fn main() {
     let args = parse_args();
     println!("{:?}", args);
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+    match fs::write(&args.output, &data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error:".bold().red(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
 }
 
 #[derive(Debug)]
