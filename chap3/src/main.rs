fbd1a419fee1d7a22a19fa58813e5590ebdff5f7
Yo Sasaki <inukichi.i@gmail.com>
2023-08-15 11:47:36 +0900

    test snippet

---
+++ chap3/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    println!("Hello, world!");
+    let v = build_vector();
+    println!("{:?}", v);
+    let v = build_vector_infer();
+    println!("{:?}", v);
+}
+
+fn build_vector() -> Vec<i16> {
+    let mut v: Vec<i16> = Vec::<i16>::new();
+    v.push(10);
+    v.push(20i16);
+    v.push(30i16);
+    v
+}
+
+fn build_vector_infer() -> Vec<i16> {
+    let mut v = Vec::new();
+    v.push(123);
+    v.push(345);
+    v.push(456);
+    v
+}
