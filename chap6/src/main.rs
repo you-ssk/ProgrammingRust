ee308be16ecc1475de3707e15d198b6dc122a8e0
Yo Sasaki <inukichi.i@gmail.com>
2023-09-06 23:17:28 +0900

    6.4

--- chap6/src/main.rs
+++ chap6/src/main.rs
@@ -1,8 +1,12 @@
 use http::StatusCode;
+use std::fs;
+use std::io;
+use std::io::BufRead;
 
 fn main() {
     println!("Hello, world!");
     ex_6_1();
+    ex_6_4();
 }
 
 fn ex_6_1() {
@@ -13,4 +17,19 @@ fn ex_6_1() {
         http::StatusCode::INTERNAL_SERVER_ERROR
     };
     println!("{} {}", a, status);
+}
+
+fn ex_6_4() -> io::Result<()> {
+    // let cont = fs::read_to_string("Cargo.toml").unwrap();
+    // for l in cont.lines(){
+    //     println!("{}", l);
+    // }
+
+    let file = fs::File::open("Cargo.toml")?;
+    let buffered = io::BufReader::new(file);
+    for line in buffered.lines() {
+        let line = line?;
+        println!("{}", line)
+    }
+    Ok(())
 }
