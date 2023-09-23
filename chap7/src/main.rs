12d17699a3307cd7ccc3af62c34c124adf209920
Yo Sasaki <inukichi.i@gmail.com>
2023-09-23 17:59:36 +0900

    7.2.5

--- chap7/src/main.rs
+++ chap7/src/main.rs
@@ -1,9 +1,16 @@
+
+use std::io;
+use std::fs;
+use std::io::BufRead;
+
+
 fn main() {
     ex_7_1();
     ex_7_2_1();
     ex_7_2_2();
     ex_7_2_3();
     ex_7_2_4();
+    ex_7_2_5();
 }
 
 fn pirate_share(total: u64, crew_size: usize) -> u64 {
@@ -17,8 +24,6 @@ fn ex_7_1() {
     let share = pirate_share(total, crew_size);
     println!("share = {}", share);
 }
-
-use std::io;
 
 fn foo() -> io::Result<()> {
     let a = 100;
@@ -58,18 +63,38 @@ fn ex_7_2_2() {
     println!("{:?}", r)
 }
 
-fn ex_7_2_3(){
+fn ex_7_2_3() {
     let r = std::fs::remove_file("a.txt");
     println!("{}", r.as_ref().err().unwrap());
     //println!("{}", r.as_ref().ok().unwrap());
 }
 
-fn bar() -> Result<u32,io::Error>{
+fn bar() -> Result<u32, io::Error> {
     let r = std::fs::remove_file("a.txt")?;
     Ok(32)
 }
 
-fn ex_7_2_4(){
+fn ex_7_2_4() {
     let r = bar();
+    println!("{:?}", r);
+}
+
+type GenericError = Box<dyn std::error::Error + Send + Sync + 'static>;
+type GenericResult<T> = Result<T, GenericError>;
+
+fn read_numbers(file: &mut dyn BufRead) -> GenericResult<Vec<i64>> {
+    let mut numbers = vec![];
+    for line_result in file.lines() {
+        let line = line_result?;
+        numbers.push(line.parse()?);
+    }
+    Ok(numbers)
+}
+
+fn ex_7_2_5() {
+    //let mut file = fs::File::open("Cargo.toml").unwrap();
+    let mut file = fs::File::open("ex7_2_5.txt").unwrap();
+    let mut bufread = io::BufReader::new(file);
+    let r = read_numbers(&mut bufread);
     println!("{:?}", r);
 }
