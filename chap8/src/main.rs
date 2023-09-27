e56d704f6af866fa68de3285df00d50d553c326b
Yo Sasaki <inukichi.i@gmail.com>
2023-09-27 22:49:35 +0900

    8.2.3

--- chap8/src/main.rs
+++ chap8/src/main.rs
@@ -1,3 +1,15 @@
+use std::mem;
+use std::collections::{HashMap, HashSet};
+use std::fs::{self, File};
+use std::mem::swap as memswap;
+
 fn main() {
     println!("Hello, world!");
+    let mut x = 5;
+    let mut y = 42;
+    println!("{} : {}", x, y);
+    mem::swap(&mut x, &mut y);
+    println!("{} : {}", x, y);
+    memswap(&mut x, &mut y);
+    println!("{} : {}", x, y);
 }
