bb9e99221cee7871ff796bb905f25e9c816415c6
Yo Sasaki <inukichi.i@gmail.com>
2023-09-30 17:26:12 +0900

    8.6

--- chap8/src/lib.rs
+++ chap8/src/lib.rs
@@ -1,6 +1,6 @@
-use std::mem;
 use std::collections::{HashMap, HashSet};
 use std::fs::{self, File};
+use std::mem;
 use std::mem::swap as memswap;
 
 fn main() {
@@ -15,7 +15,7 @@ fn main() {
 
     let mut fern = Fern {
         size: 1.0,
-        growth_rate: 0.001
+        growth_rate: 0.001,
     };
     run_simulate(&mut fern, 1000);
     println!("final fern size: {}", fern.size);
@@ -23,7 +23,7 @@ fn main() {
 
 pub struct Fern {
     pub size: f64,
-    pub growth_rate: f64
+    pub growth_rate: f64,
 }
 
 impl Fern {
@@ -35,5 +35,40 @@ impl Fern {
 pub fn run_simulate(fern: &mut Fern, days: usize) {
     for _ in 0..days {
         fern.grow();
+    }
+}
+
+#[test]
+fn math_works() {
+    let x: i32 = 1;
+    assert!(x.is_positive());
+    assert_eq!(x + 1, 2);
+}
+
+#[test]
+#[allow(unconditional_panic, unused_must_use)]
+#[should_panic(expected = "divide by zero")]
+fn test_divide_by_zero_error() {
+    1 / 0;
+}
+
+use std::num::ParseIntError;
+
+#[test]
+fn explicit_radix() -> Result<(), ParseIntError> {
+    i32::from_str_radix("1024", 10)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    fn roughly_equal(a: f64, b: f64) -> bool {
+        (a - b).abs() < 1e-6
+    }
+
+    #[test]
+    fn trig_works() {
+        use std::f64::consts::PI;
+        assert!(roughly_equal(PI.sin(), 0.0));
     }
 }
---
+++ chap8_6_3/ranges/src/lib.rs
@@ -0,0 +1,29 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+use std::ops::Range;
+
+/// Return true if two ranges overlap;
+/// 
+///     assert_eq!(ranges::overlap(0..7, 3..10), true);
+///     assert_eq!(ranges::overlap(1..5, 101..105), false);
+/// 
+/// If either range is empty, they don't cout as overlapping.
+/// 
+///     assert_eq!(ranges::overlap(0..0, 0..10), false);
+/// 
+pub fn overlap(r1: Range<usize>, r2: Range<usize>) -> bool {
+    r1.start < r1.end && r2.start < r2.end && r1.start < r2.end && r2.start < r1.end
+}
