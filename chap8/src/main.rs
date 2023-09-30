e43e02f00f129dc4b98a5edbb6d0263283e96aaf
Yo Sasaki <inukichi.i@gmail.com>
2023-09-30 14:35:41 +0900

    8.3

--- chap8/src/main.rs
+++ chap8/src/main.rs
@@ -12,4 +12,28 @@ fn main() {
     println!("{} : {}", x, y);
     memswap(&mut x, &mut y);
     println!("{} : {}", x, y);
+
+    let mut fern = Fern {
+        size: 1.0,
+        growth_rate: 0.001
+    };
+    run_simulate(&mut fern, 1000);
+    println!("final fern size: {}", fern.size);
+}
+
+struct Fern {
+    size: f64,
+    growth_rate: f64
+}
+
+impl Fern {
+    fn grow(&mut self) {
+        self.size *= 1.0 + self.growth_rate;
+    }
+}
+
+fn run_simulate(fern: &mut Fern, days: usize) {
+    for _ in 0..days {
+        fern.grow();
+    }
 }
