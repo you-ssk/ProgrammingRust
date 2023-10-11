3f3e5edb23451784a2fd0832f30e45b9ff6172f4
Yo Sasaki <inukichi.i@gmail.com>
2023-10-11 22:34:22 +0900

    9.8

--- chap9/src/main.rs
+++ chap9/src/main.rs
@@ -14,6 +14,7 @@ fn main() {
 
     ex_9_6();
     ex_9_7();
+    ex_9_8();
 }
 
 fn new_map(size: (usize, usize), pixels: Vec<u8>) -> grayscale::GrayscaleMap {
@@ -152,4 +153,28 @@ fn ex_9_7() {
     r.push(13764.0);
     println!("{:?}", q);
     println!("{:?}", r);
+}
+
+fn ex_9_8() {
+    struct Extrema<'elt> {
+        greatest: &'elt i32,
+        least: &'elt i32,
+    }
+    fn find_extrema<'s>(slice: &'s [i32]) -> Extrema<'s> {
+        let mut greatest = &slice[0];
+        let mut least = &slice[0];
+        for i in 1..slice.len() {
+            if slice[i] < *least {
+                least = &slice[i];
+            };
+            if slice[i] > *greatest {
+                greatest = &slice[i];
+            }
+        }
+        Extrema { greatest, least}
+    }
+    let a = [0,-3, 0, 15, 48];
+    let e = find_extrema(&a);
+    assert_eq!(*e.least, -3);
+    assert_eq!(*e.greatest, 48);
 }
