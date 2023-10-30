59239c993be02ce1e076c5ff768144e3e7896d20
Yo Sasaki <inukichi.i@gmail.com>
2023-10-30 23:19:18 +0900

    beginning of chap 11.

--- chap11/src/main.rs
+++ chap11/src/main.rs
@@ -1,3 +1,22 @@
+use std::io::Write;
+
 fn main() {
     println!("Hello, world!");
+    ex_11_0();
+}
+
+fn say_hello(out: &mut dyn Write) -> std::io::Result<()> {
+    out.write_all(b"hello world\n")?;
+    out.flush()
+}
+
+fn ex_11_0() -> std::io::Result<()>{
+    use std::fs::File;
+    let mut local_file = File::create("hello.txt")?;
+    say_hello(&mut local_file)?;
+
+    let mut bytes = vec![];
+    say_hello(&mut bytes)?;
+    assert_eq!(bytes, b"hello world\n");
+    Ok(())
 }
