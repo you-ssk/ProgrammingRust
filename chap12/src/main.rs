6ef6faab0e1733e7047f6e93a213d153da1919ca
Yo Sasaki <inukichi.i@gmail.com>
2023-12-05 23:18:06 +0900

    complex.

--- chap12/src/main.rs
+++ chap12/src/main.rs
@@ -1,3 +1,12 @@
+use num::Complex;
+
 fn main() {
     println!("Hello, world!");
+    let c = Complex { re: 1.0, im: -0.0625 };
+    println!("{}", c);
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..10 {
+        z = z * z + c;
+        println!("{}", z);
+    }
 }
