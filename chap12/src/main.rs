5f4fdddd9e3be10a5d1096f0cb57fca9eac3a2f1
Yo Sasaki <inukichi.i@gmail.com>
2023-12-10 22:32:12 +0900

    12.1.1

--- chap12/src/main.rs
+++ chap12/src/main.rs
@@ -2,23 +2,52 @@ use num::Complex;
 
 fn main() {
     println!("Hello, world!");
-    let c = Complex { re: 1.0, im: -0.0625 };
+    let c = Complex {
+        re: 1.0,
+        im: -0.0625,
+    };
     println!("{}", c);
     let mut z = Complex { re: 0.0, im: 0.0 };
-    for i in 0..10 {
-        z = z * z + c;
-        println!("{}", z);
-    }
+    // for i in 0..10 {
+    //     z = z * z + c;
+    //     println!("{}", z);
+    // }
 
     ex_12_1();
+    ex_12_1_1();
 }
 
-use std::ops::Add;
+use std::{ascii::EscapeDefault, ops::Add};
 
 fn ex_12_1() {
-    let mut c = Complex { re: 1.0, im: -0.0625 };
+    let mut c = Complex {
+        re: 1.0,
+        im: -0.0625,
+    };
     let mut z = Complex { re: 0.0, im: 0.0 };
     z = z + c;
     z = z.add(c);
+    println!("{}", z);
+}
+
+use std::ops::Neg;
+
+// impl<T> Neg for Complex<T>
+// where
+//     T: Neg<Output = T>,
+// {
+//     type Output = Complex<T>;
+//     fn neg(self) -> Complex<T> {
+//         Complex {
+//             re: -self.re,
+//             im: -self.im,
+//         }
+//     }
+// }
+
+fn ex_12_1_1() {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    println!("{}", z);
+    z = z.neg();
     println!("{}", z);
 }
