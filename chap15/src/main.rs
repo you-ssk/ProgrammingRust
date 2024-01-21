450eeaaef12ff6c0cb958c91a883df0acf5faa8c
Yo Sasaki <inukichi.i@gmail.com>
2024-01-21 15:47:21 +0900

    15.1

--- chap15/src/main.rs
+++ chap15/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    fn triangle(n: i32)->i32 {
+    fn triangle(n: i32) -> i32 {
         let mut sum = 0;
         for i in 1..=n {
             sum += i;
@@ -12,4 +12,20 @@ fn main() {
         (1..=n).fold(0, |sum, item| sum + item)
     }
     println!("{}", triangle2(10));
+
+    ex_15_1();
+}
+
+fn ex_15_1() {
+    println!("There is :");
+    let v = vec!["antimony", "arsenic", "aluminum", "selenium"];
+    for element in &v {
+        println!("{}", element);
+    }
+
+    println!("There is :");
+    let mut iterator = (&v).into_iter();
+    while let Some(element) = iterator.next() {
+        println!("{}", element);
+    }
 }
