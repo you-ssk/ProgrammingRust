ce40df9bf2cfdce9616929d97479818e759baada
Yo Sasaki <inukichi.i@gmail.com>
2024-02-04 15:02:21 +0900

    15.3.2

--- chap15/src/main.rs
+++ chap15/src/main.rs
@@ -106,6 +106,7 @@ fn ex_15_2_4() {
 
 fn ex_15_3() {
     ex_15_3_1();
+    ex_15_3_2();
 }
 
 fn ex_15_3_1() {
@@ -125,4 +126,38 @@ fn ex_15_3_1() {
     // {
     //     ["earth", "water", "air", "fire"].iter().map(|elt| println!("{}", elt));
     // }
+}
+
+fn ex_15_3_2() {
+    use std::str::FromStr;
+
+    let text = "1\nfrond .25 289\n3.1415 estuary\n";
+    for number in text
+        .split_whitespace()
+        .filter_map(|w| f64::from_str(w).ok())
+    {
+        println!("{:4.2}", number.sqrt());
+    }
+
+    let nums: Vec<f64> = text
+        .split_whitespace()
+        .map(|w| f64::from_str(w))
+        .filter(|r| r.is_ok())
+        .map(|r| r.unwrap().sqrt())
+        .collect();
+    println!("{:?}", nums);
+
+    use std::collections::HashMap;
+
+    let mut major_cities = HashMap::new();
+    major_cities.insert("Japan", vec!["Tokyo", "Kyoto"]);
+    major_cities.insert("The United States", vec!["Portland", "Nashville"]);
+    major_cities.insert("Brazil", vec!["Sao Paulo", "Brasilia"]);
+    major_cities.insert("Kenya", vec!["Nairobi", "Mombasa"]);
+    major_cities.insert("The Netherlands", vec!["Amsterdam", "Utrecht"]);
+    let countries = ["Japan", "Brazil", "Kenya"];
+
+    for &city in countries.iter().flat_map(|country| &major_cities[country]) {
+        println!("{}", city);
+    }
 }
